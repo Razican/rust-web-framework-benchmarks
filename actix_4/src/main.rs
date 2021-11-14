19618d9979290cdb770b9d51de03c63d8b464f72
Iban Eguia Moraza <razican@protonmail.ch>
2021-11-14 19:21:20 +0100

    Added Actix v3-v4 benchmarks

---
+++ actix_4/src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn ping(_req: HttpRequest) -> impl Responder {
+    "pong"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let _ = dotenv::dotenv().ok();
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/ping", web::get().to(ping))
+    })
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
+}
